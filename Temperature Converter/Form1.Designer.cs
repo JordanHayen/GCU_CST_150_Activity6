6a887998d77e53d421f784fa1ef80c0d06729fa8
JordanHayen <jordan.hayen@gmail.com>
2021-10-10 14:51:33 -0700

    The files for Activity 6

---
+++ Temperature Converter/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Temperature_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Convert = new System.Windows.Forms.Button();
+            this.Fahrenheit = new System.Windows.Forms.TextBox();
+            this.Celsius = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(120, 155);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(100, 23);
+            this.Convert.TabIndex = 0;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Fahrenheit
+            // 
+            this.Fahrenheit.Location = new System.Drawing.Point(120, 73);
+            this.Fahrenheit.Name = "Fahrenheit";
+            this.Fahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.Fahrenheit.TabIndex = 3;
+            this.Fahrenheit.TextChanged += new System.EventHandler(this.Fahrenheit_TextChanged);
+            // 
+            // Celsius
+            // 
+            this.Celsius.Location = new System.Drawing.Point(120, 114);
+            this.Celsius.Name = "Celsius";
+            this.Celsius.Size = new System.Drawing.Size(100, 20);
+            this.Celsius.TabIndex = 4;
+            this.Celsius.TextChanged += new System.EventHandler(this.Celsius_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fahrenheit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Celsius:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(172)))), ((int)(((byte)(129)))));
+            this.ClientSize = new System.Drawing.Size(342, 350);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Celsius);
+            this.Controls.Add(this.Fahrenheit);
+            this.Controls.Add(this.Convert);
+            this.Name = "Form1";
+            this.Text = "Temperature Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.TextBox Fahrenheit;
+        private System.Windows.Forms.TextBox Celsius;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Temperature Converter/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature_Converter
+{
+    public partial class Form1 : Form
+    {
+
+        string activeTextBox; // a string that contains the name of the last textbox to be edited
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /* This method is called when the Convert button is clicked */
+        private void Convert_Click(object sender, EventArgs e)
+        {
+
+            double temperatureInput; // a variable that stores the temperature to be converted
+
+            try // Made with help from the textbook (Gaddis, 160)
+            {
+                // This switch determines which operation to perform depending on which was the last textbox to have been edited
+                switch (activeTextBox)
+                {
+                    // this code block runs if the last textbox to be edited was the Fahrenheit textbox
+                    case "Fahrenheit":
+                        temperatureInput = Double.Parse(Fahrenheit.Text); // Converts the entry in the Fahrenheit textbox into a double. If this is not possible, an exception is caught
+                        Celsius.Text = FormatResult(FahrenheitToCelsius(temperatureInput)); // converts the the entry in the Fahrenheit textbox to Celsius and displays the formatted result in the Celsius textbox
+                        break;
+                    // this code block runs if the last textbox to be edited was the Celsius textbox
+                    case "Celsius":
+                        temperatureInput = Double.Parse(Celsius.Text); // Converts the entry in the Celsius textbox into a double. If this is not possible, an exception is caught
+                        Fahrenheit.Text = FormatResult(CelsiusToFahrenheit(temperatureInput)); // converts the the entry in the Celsius textbox to Fahrenheit and displays the formatted result in the Fahrenheit textbox
+                        break;
+                }
+            }
+            catch(Exception ex) // If the input cannot be parsed into a numeric data type, an exception will result. This displays the exception's message
+            {
+                MessageBox.Show(ex.Message); // Displays the exception's message
+            }
+
+            FormatResult(123.1);
+        }
+
+        /* This method is called when the Fahrenheit textbox is edited */
+        private void Fahrenheit_TextChanged(object sender, EventArgs e)
+        {
+            activeTextBox = "Fahrenheit"; // changes the activeTextBox variable to "Fahrenheit"
+        }
+
+        /* This method is called when the Celsius textbox is edited */
+        private void Celsius_TextChanged(object sender, EventArgs e)
+        {
+            activeTextBox = "Celsius"; // changes the activeTextBox variable to "Celsius"
+        }
+
+        /* Converts a double input from Fahrenheit to Celsius */
+        private double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;  // uses the input in the formula to convert Fahrenheit to Celsius and returns the result. This formula comes from the textbook (Gaddis, 197).
+        }
+
+        /* Converts a double input from Celsius to Fahrenheit */
+        private double CelsiusToFahrenheit(double celsius)
+        {
+            return celsius * 1.8 + 32; // uses the input in the formula to convert Celsius to Fahrenheit and returns the result. This formula comes from the textbook (Gaddis, 197).
+        }
+
+        // This method takes in a double method and formats it so that it contains 3 decimal places, returning the result as a string
+        private string FormatResult(double number)
+        {
+            string decimalPlaces = ""; // A string variable representing the decimal places of the number
+            string result = ""; // A string variable that will contain the eventual return value of the method
+
+            // Adds the decimal places of the parameter to the decimalPlaces variable
+            bool decimalPointDetected = false; // A boolean variable that will be set to true once the following for-loop reaches the decimal point in the parameter
+            for(int i = 0; i < number.ToString().Length; i++) // This for-loop iterates once for every character in the parameter after it has been parsed into a string
+            {
+                if(!decimalPointDetected) // If the decimal point has not been detected yet, the loop should not add the current character to the decimalPlaces variable
+                {
+                    if(number.ToString()[i] == '.') // If the current character is a decimal point, set the decimalPointDetected variable to true
+                    {
+                        decimalPointDetected = true;
+                    }
+                }
+                else // If the decimal point has been detected, each character afterward should be added to the decimalPlaces variable
+                {
+                    decimalPlaces += number.ToString()[i];
+                }
+            }
+
+            // In order to be properly formatted, the result should have exactly three decimal places. If it has more or fewer then it must be rounded or zeroes must be added onto the end
+            if(decimalPlaces.Length < 3) // If there are fewer than three decimal places then zeroes must be added until there are
+            {
+                while(decimalPlaces.Length < 3) // This loop iterates until the decimalPlaces variable has a length of 3
+                {
+                    decimalPlaces += "0"; // A zero is added to the decimalPlaces variable
+                }
+            } 
+            else if(decimalPlaces.Length > 3) // If there are more than three decimal places then the parameter must be rounded
+            {
+                if(int.Parse(decimalPlaces[3].ToString()) >= 5) // If the fourth decimal place is greater than or equal to 5 then the third decimal place must be increased by 1
+                {
+                    decimalPlaces = decimalPlaces[0].ToString() + decimalPlaces[1] + (int.Parse(decimalPlaces[2].ToString()) + 1).ToString(); // Sets the decimalPlaces variable to the first three decimal places, adding 1 to the third
+                }
+                else // If the fourth decimal place is less than 5 then the third decimal place does not need to be increased
+                {
+                    decimalPlaces = decimalPlaces[0].ToString() + decimalPlaces[1] + decimalPlaces[2]; // Sets the decimalPlaces variable to the first three decimal places
+                }
+            }
+
+            result = ((int)number).ToString() + "." + decimalPlaces; // Sets the result variable to a formatted string
+            return result; // Returns the result variable
+        }
+    }
+}
+
+// References:
+// Gaddis, T. (2020). Starting Out With Visual C#. Pearson.
